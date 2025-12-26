21fda33f9450d2ab15d89b4d76481044b33fb661
JaimeFine <jaimefine6@gmail.com>
2025-12-26 14:46:26 +0800

    new file:   notebooks/motion_prediction.ipynb 	renamed:    notebooks/MyThoughts-POIs.ipynb -> notebooks/poi_detection.ipynb 	modified:   pyscripts/processor.py 	new file:   pyscripts/structure_lib.py

--- pyscripts/processor.py
+++ pyscripts/processor.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 import numpy as np
 from datetime import datetime
 import json
+# from structure_lib import Node, LinkedList
 
 # -------------------- Block 1 ----------------- # 
 #      Preprocessing with the flight data        #
@@ -10,10 +11,11 @@ import json
 flights = defaultdict(lambda: {
     "coords": [],
     "vel": [],
-    "time": []
+    "time": [],
+    "dt": []
 })
 
-with open("") as raw:
+with open("D:/ADataBase/flights_data_geojson/2024-11-10/2024-11-10-CAN_processed.geojson") as raw:
     geojson = json.load(raw)
 
 for feature in geojson["features"]:
@@ -21,12 +23,13 @@ for feature in geojson["features"]:
     geom = feature["geometry"]
     
     f_id = props["flight_id"]
+    dt = props["dt"]
 
     lon, lat, alt = geom["coordinates"]
     vx, vy, vz = map(float, props["velocity"].split())
 
     timestamp = datetime.fromisoformat(props["timestamp"])
-
+    
     flights[f_id]["coords"].append([lon, lat, alt])
     flights[f_id]["vel"].append([vx, vy, vz])
     flights[f_id]["time"].append(timestamp)
@@ -34,13 +37,22 @@ for feature in geojson["features"]:
 for f_id in flights:
     flights[f_id]["coords"] = np.array(flights[f_id]["coords"])
     flights[f_id]["vel"] = np.array(flights[f_id]["vel"])
+    flights[f_id]["dt"] = np.array(flights[f_id]["dt"])
 
 # ------------------ Block 2 ----------------- # 
 #            Computation heavy zone            #
 # -------------------------------------------- #
+"""
+from julia import Julia
+jl = Julia(compiled_modules=False)
+from julia import LinearAlgebra
+"""
 
-# I decide to use a lovely linked list structure for the loss!
+# Pure physics-based model:
+physic_matrix = np.array(len(flights[f_id]), len(flights[f_id]["coords"] - 1))
 for data in flights:
-    for i in range(len(flights[data]["coords"])):
-        flights[data]["coords"][i]
-        # Hold on...
+    for i in range(len(flights[data]["coords"] - 1)):
+        # The phsic_matrix's first column is the prediction for 2nd position!!!
+        dx = flights[data]["coords"][i] + flights[data]["vel"][i+1] @ flights[data]["dt"][i+1]
+        physic_matrix[data][i] = dx
+        
---
+++ pyscripts/structure_lib.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, value):
+        node = Node(value)
+        if not self.head:
+            self.head = node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = node
