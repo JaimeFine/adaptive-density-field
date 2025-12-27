a9649f1bae4f7003b6eb904939dea4830656dcbb
JaimeFine <jaimefine6@gmail.com>
2025-12-27 14:19:36 +0800

    Update

--- pyscripts/processor.py
+++ pyscripts/processor.py
@@ -59,11 +59,26 @@ for f_id in flights:
     size = len(coords) - 1
     physic_normal[f_id] = np.zeros((size, 3), dtype = float)
 
-    for i in range(size):
+    for i in range(size - 1):
         # The phsic_matrix's first column is the prediction for 2nd position!!!
         dx = coords[i] + vel[i+1] * dt[i+1]
         physic_normal[f_id][i] = dx
         
+"""
+Obviously, the output:
+...
+[ 3.93346485e+04, -9.70215903e+04,  3.61000000e+04],
+[ 1.97690042e+04, -4.85948999e+04,  3.61000000e+04],
+[ 3.93348216e+04, -9.70219568e+04,  3.61000000e+04],
+[-5.31731053e+04, -7.33233705e+04,  3.61000000e+04],
+[-3.95573973e+04, -1.84714838e+04,  3.99400000e+04],
+[-3.95575754e+04, -1.84716463e+04,  3.61000000e+04],
+[-3.42670276e+04, -1.60046179e+04,  3.61000000e+04],
+[-1.35313544e+04, -6.92548368e+03,  3.61000000e+04],
+...
+is stupid!
+"""
+
 # Pure physics-based model ----- advanced:
 better_matrix = np.array()
 
